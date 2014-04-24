f449adacc1e751a4786974af98d7bc795b261532
Paul Cher <paulcher@onsec.ru>
2014-04-25 00:34:56 +0400

    Commiting the config file

---
+++ config.py
@@ -0,0 +1,16 @@
+# Config file for this amazing system
+
+import sys
+
+host = "0.0.0.0"
+port = 8088
+
+sys.dont_write_bytecode = True
+
+tasks_enabled = 0
+scoreboard_enabled = 1
+hints_enabled = 0
+
+default_modules = ['__builtins__', '__doc__', '__file__', '__name__', '__package__']
+
+
