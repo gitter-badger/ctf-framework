df4af5501210dda523525a3d84d8289c680f2567
Paul Cher <paulcher@onsec.ru>
2014-04-25 22:18:01 +0400

    Edited configs

--- config.py
+++ config.py
@@ -2,14 +2,18 @@
 
 import sys
 
-host = "0.0.0.0"
+host = "0.0.0.0" # broadcast range
+host_ip = "10.0.0.1" # ip_addr of interface you are currently using
 port = 8088
+tasks_port = 8888
+
+
 
 sys.dont_write_bytecode = True
 
-tasks_enabled = 0
+tasks_enabled = 1
 scoreboard_enabled = 1
-hints_enabled = 0
+hints_enabled = 1
 
 default_modules = ['__builtins__', '__doc__', '__file__', '__name__', '__package__']
 
