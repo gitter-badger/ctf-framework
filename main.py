afee38acf522f700858ed8714483b6c263a4fbba
Paul Cher <paulcher@onsec.ru>
2014-04-27 22:53:51 +0400

    logs, tasks, queries

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import hints
 import loader
 import config
 import logging
+import hashlib
 
 import sqlite3 as sql
 
@@ -17,6 +18,7 @@ from flask import Flask, request, session, \
     render_template, flash
 
 from urlparse import urlparse, parse_qs
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
@@ -24,60 +26,10 @@ ttypes = loader.load_task_types()
 subttypes = loader.load_task_subtypes(ttypes)
 
 
-#
-# This part is NOT FINISHED YET
-# TO DEL
-
-#     def do_POST(self):
-#         if (config.tasks_enabled):
-#             q = urlparse(self.path)
-#             args = parse_qs(q.query)
-
-#             length = int(self.headers.getheader('content-length'))
-#             postvars = parse_qs(self.rfile.read(length))
-
-#             if (postvars.has_key('team_name') and postvars.has_key('flag')):
-#                 with open ('/'. join(['tasks/', args['t'][0],args['c'][0], 'desc']), 'r') as f:
-#                     o = f.readlines()
-#                     self.send_response(301)
-
-#                     connection = sql.connect('score.db')
-#                     q = "select team_name from score where flag = \'%s\' and team_name = \'%s\';"
-#                     res = connection.execute(q % (postvars['flag'][0], postvars['team_name'][0]))
-#                     for row in res:
-#                         if (row):
-#                             self.send_header('Location', 'index?r=already_added')
-#                             break
-#                     else:
-#                         if(o[3].strip('\n') == postvars['flag'][0]):
-#                             q = 'insert into score values (\'%s\', \'%s\', %s, \'%s\');'
-#                             connection.execute(q % (postvars['team_name'][0],
-#                                 postvars['flag'][0],
-#                                 args['c'][0],
-#                                 time.strftime('%Y-%m-%d %H:%M:%S'))
-#                                                 )
-#                             connection.commit()
-                            
-#                             self.send_header('Location', 'index?r=success')
-#                         else:
-#                             self.send_header('Location', 'index?r=fail')
-
-#                     connection.close()
-
-#                 self.end_headers()
-
-#             else:
-#                 self.send_response(200)
-#                 self.send_header('Content-type', 'text/html')
-#                 self.end_headers()
-#         else:
-#             document = 'You are trying to submit flag after CTF is over. The incedent will be reported!'
-#             self.send_response(200)
-#             self.send_header('Content-type', 'text/html')
-#             self.end_headers()
-#             self.wfile.write(document)
-
-#     notice = ""
+def not_base_mod (module):
+    if (module in config.base_modules):
+        return False
+    return True
 
 def ftitle(var_title):
     return {
@@ -87,12 +39,7 @@ def ftitle(var_title):
     }[var_title]
 
 def fhead(var_title):
-    return '\n'.join([head, (menu % ftitle(var_title)), (title % var_title)])
-
-def not_base_mod (module):
-    if (module in config.base_modules):
-        return False
-    return True
+    return ''.join([head, (menu % ftitle(var_title)), (title % var_title)])
 
 def fhints():
     if (not config.hints_enabled):
@@ -103,8 +50,16 @@ def fhints():
     if (config.hints_enabled) :
         reload(hints)
         for var_hint in filter(not_base_mod,  dir(hints)):  
-            rhint += snipp_hint % hints.rglobals()[var_hint]
+            rhint += (snipp_hint % hints.rglobals()[var_hint])
     return rhint + hint_bottom
+
+def fnotice (notice):
+    return {
+        'success': flag_added,
+        'fail': flag_declined,
+        'already_added': flag_already_been_added,
+        '' : ''
+    }[notice]
 
 @app.route('/scoreboard')
 def scoreboard ():
@@ -125,18 +80,62 @@ def scoreboard ():
     return document + scoreboard_footer
 
     
-@app.route('/task/<task_name>/<int:cost>')
-def task(task_name, cost):
-    with open ( '/'.join( ['tasks', task_name, cost, 'desc' ] ), 'r') as f:
-            return '\n'.join([head,    submit_bar,
-                                menu % ('', 'active', ''), (title % task_name), 
-                                task_description.format( f.readline().strip('\n'),
-                                    f.readline().strip('\n'),
-                                    f.readline().strip('\n'),
-                                    host_ip,
-                                    tasks_port,
-                                    '/'.join([task_name, cost]))]
-                            )
+@app.route('/task/<task_type>/<int:cost>', methods=['GET', 'POST'])
+def task(task_type, cost):
+    if request.method == 'POST':
+        return commit_flag(task_type, cost)
+    elif request.method == 'GET':
+        return show_task(task_type, cost)
+
+def commit_flag(task_type, cost):
+    if (not config.tasks_enabled):
+        return 'You are trying to submit flag after CTF is over. The incedent will be reported!'
+
+    with open ('/'. join(['tasks/', task_type, str(cost), 'desc']), 'r') as f:
+        o = f.readlines()
+
+        team_name = request.form['team_name']
+        flag = hashlib.new('md5')
+        flag.update(request.form['flag'])
+
+        connection = sql.connect('score.db')
+
+        q = "select team_name from score where task_type = \'%s\' and cost = \'%s\' and team_name = \'%s\';"
+
+        res = [r for r in connection.execute(q % (task_type, cost, team_name))]
+        print res
+
+        if res:
+                return show_task(task_type, cost, notice='already_added')
+        else:
+            if(o[3].strip('\n') == flag.hexdigest()):
+                q = 'insert into score values (\'%s\', \'%s\', %s, \'%s\');'
+                connection.execute(q % (team_name,
+                    task_type,
+                    cost,
+                    time.strftime('%Y-%m-%d %H:%M:%S'))
+                )
+                connection.commit()
+                connection.close()
+                return show_task(task_type, cost, notice='success')
+
+            else:
+                connection.close()
+                return show_task(task_type, cost, notice='fail')
+    
+def show_task(task_type, cost, notice=''):
+    with open ( '/'.join( ['tasks', task_type, str(cost), 'desc' ] ), 'r') as f:
+        return ''.join([fhead('TASKS'),
+                            submit_bar,
+                            fnotice(notice),
+                            task_description.format( f.readline().strip('\n'),
+                                f.readline().strip('\n'),
+                                f.readline().strip('\n'),
+                                config.host_ip,
+                                config.tasks_port,
+                                '/'.join([task_type, str(cost)]))]
+                        )
+
 
 @app.route('/tasks')
 def tasks():
@@ -163,7 +162,7 @@ def tasks():
                                     f.readline().strip('\n')
                                     )
                                 )
-            document += task_row_f
+        document += task_row_f
     return document + div_row_e + footer
 
 
@@ -173,4 +172,7 @@ def index():
     return fhead('HOME') + home + footer
 
 if __name__ == '__main__':
+    handler = RotatingFileHandler(config.errorlog, maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run(host=config.host, port=config.port)
