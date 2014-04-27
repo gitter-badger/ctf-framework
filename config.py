04b1ec6703b2b2a1e2b453aad833e0f63537634b
Paul Cher <paulcher@onsec.ru>
2014-04-27 22:52:56 +0400

    Error logs created

--- config.py
+++ config.py
@@ -1,21 +1,17 @@
 # Config file for this amazing system
 
-import sys
-
 host = "0.0.0.0" # broadcast range
 host_ip = "10.0.0.1" # ip_addr of interface you are currently using
 port = 8088
 tasks_port = 8888
 
-sys.dont_write_bytecode = True
-
 tasks_enabled = 1
 scoreboard_enabled = 1
 hints_enabled = 1
 
-base_modules = ['__builtins__', '__doc__', '__file__', '__name__', '__package__']
+base_modules = ['__builtins__', '__doc__', '__file__', '__name__', '__package__', 'rglobals']
 
 
-accesslog = '/var/log/msuctf-access.log'
-errorlog = '/var/log/msuctf-error.log'
+accesslog = 'logs/msuctf-access.log'
+errorlog = 'logs/msuctf-error.log'
 
