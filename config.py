7b3902776cd799f3ad2ba771b9943299434b3deb
Paul Cher <paulcher@onsec.ru>
2014-04-27 11:47:00 +0400

    no more sht functions with db

--- config.py
+++ config.py
@@ -19,17 +19,3 @@ base_modules = ['__builtins__', '__doc__', '__file__', '__name__', '__package__'
 accesslog = '/var/log/msuctf-access.log'
 errorlog = '/var/log/msuctf-error.log'
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
- 
