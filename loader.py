c03c1f5951f81108880b79820da8bfcd117907cd
Paul Cher <paulcher@onsec.ru>
2014-04-28 23:16:17 +0400

    Cache added

--- loader.py
+++ loader.py
@@ -6,8 +6,21 @@ def load_task_types():
     return [item for item in os.listdir('tasks') if os.path.isdir('tasks' '/' + item)]
 
 def load_task_subtypes(ttypes):
-	r = {}
-	for ttype in ttypes: 
-		r[ttype] = [item for item in os.listdir('tasks' '/' + ttype) if os.path.isdir( '/'.join(['tasks', ttype,  item]))]
+    r = {}
+    for ttype in ttypes: 
+        r[ttype] = [item for item in os.listdir('tasks' '/' + ttype) if os.path.isdir( '/'.join(['tasks', ttype,  item]))]
+    return r
 
-	return r
+def load_file_cache(ttypes, subttypes):
+    cache = { } 
+    for ttype in subttypes.keys():
+        for subttype in subttypes[ttype]:
+            try:
+                with open ('/'.join(['tasks', ttype, subttype, 'desc'])) as f:
+                    cache[ttype] = { }
+                    cache[ttype][subttype] = {'filename': f.readline().strip('\n'), 'taskname': f.readline().strip('\n'),
+                                        'description': f.readline().strip('\n'), 'flag': f.readline().strip('\n')
+                                    }
+            except IOError:
+                pass
+    return cache
